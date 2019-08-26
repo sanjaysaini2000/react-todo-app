0fca2de1b71742696c1ead3fc7e96dfb453dee8d
sanjaysaini2000 <sanjaysaini2000@hotmail.com>
2019-08-26 17:41:51 +0530

    removed dead code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./components/home";
-import EditItem from "./components/editItem";
 
 export default class App extends Component {
   render() {
@@ -10,7 +9,6 @@ export default class App extends Component {
       <Router>
         <div>
           <Route path="/" exact strict component={Home} />
-          <Route path="/edit/:item" exact strict component={EditItem} />
         </div>
       </Router>
     );
